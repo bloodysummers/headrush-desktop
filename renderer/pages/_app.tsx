79f9d029de2287e91c590b1e094edbb2615af4b5
Luis Fernando Díaz <ldiaz@occ.com.mx>
2023-04-08 14:26:35 -0600

    fix: Import global styles

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import '../styles/globals.css'
 
 const queryClient = new QueryClient()
 
