c7515249f6b2ef8b3303fa620873e2e7d0197bc6
Luis Fernando Díaz <ldiaz@occ.com.mx>
2023-04-10 10:00:04 -0600

    feat: Add a drag and drop functionality to edit setlists

--- renderer/components/editable-setlist/index.tsx
+++ renderer/components/editable-setlist/index.tsx
@@ -1,16 +1,71 @@
+import { useState } from 'react'
+import {
+  DragDropContext,
+  Draggable,
+  DropResult,
+  Droppable
+} from 'react-beautiful-dnd'
 import { SetlistWithFullRigs } from '@/types/setlist'
 import ListItem from './list-item'
+import { Rig } from '@/types/rig'
 
 export default function EditableSetlist({
   items
 }: {
   items: SetlistWithFullRigs
 }) {
+  const [editableItems, setEditableItems] = useState(items.rigs_data)
+
+  const handleDrop = (droppedItem: DropResult) => {
+    if (!droppedItem.destination) return
+    var updatedList = [...editableItems]
+    const [reorderedItem] = updatedList.splice(droppedItem.source.index, 1)
+    updatedList.splice(droppedItem.destination.index, 0, reorderedItem)
+    setEditableItems(updatedList)
+  }
+
+  const handleDelete = (id: Rig) => {
+    const itemIndex = editableItems.findIndex(item => item.id === id.id)
+    if (itemIndex === -1) return
+    const updatedList = [...editableItems]
+    updatedList.splice(itemIndex, 1)
+    setEditableItems(updatedList)
+  }
+
   return (
     <div className="p-4 overflow-y-auto scrollbar-thin h-full scrollbar-track-neutral-600 scrollbar-thumb-presetGreen scrollbar-thumb-rounded-md">
-      {items.rigs_data.map(rig => {
-        return <ListItem key={rig.id} item={rig} />
-      })}
+      <DragDropContext onDragEnd={handleDrop}>
+        <Droppable droppableId="list-container">
+          {provided => (
+            <div
+              className="list-container"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {editableItems.map((rig, index) => {
+                return (
+                  <Draggable key={rig.id} draggableId={rig.id} index={index}>
+                    {provided => (
+                      <div
+                        className="item-container h-16"
+                        ref={provided.innerRef}
+                        {...provided.dragHandleProps}
+                        {...provided.draggableProps}
+                      >
+                        <ListItem
+                          item={rig}
+                          onDelete={() => handleDelete(rig)}
+                        />
+                      </div>
+                    )}
+                  </Draggable>
+                )
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   )
 }
--- renderer/components/editable-setlist/list-item/index.tsx
+++ renderer/components/editable-setlist/list-item/index.tsx
@@ -2,16 +2,27 @@ import colors from '@/tokens/colors'
 import colorsCatalog from '@/tokens/catalogs/colors'
 import { Rig } from '@/types/rig'
 import Image from 'next/image'
+import { MouseEvent } from 'react'
 
-export default function ListItem({ item }: { item: Rig }) {
-  const color = item.color
+export default function ListItem({
+  item,
+  onDelete
+}: {
+  item: Rig
+  onDelete: () => void
+}) {
+  const _onDelete = (e: MouseEvent<HTMLImageElement>) => {
+    e.stopPropagation()
+    if (onDelete) onDelete()
+  }
+
   return (
-    <div className="bg-slate-200 text-neutral-900 flex shadow-md mb-4 rounded-lg">
+    <div className="bg-slate-200 text-neutral-900 flex shadow-md rounded-lg">
       <div
         className="w-6 mr-1 rounded-l-md"
-        style={{ background: colors[colorsCatalog[color]] }}
+        style={{ background: colors[colorsCatalog[item.color]] }}
       />
-      <div className="p-2 flex mr-2 cursor-grab">
+      <div className="p-2 flex mr-2">
         <Image
           src="/img/draggable-button.svg"
           width={16}
@@ -20,7 +31,10 @@ export default function ListItem({ item }: { item: Rig }) {
         />
       </div>
       <div className="px-2 py-4 flex-1">{item.name}</div>
-      <div className="bg-red-500 w-8 h-8 flex justify-center items-center rounded-full mr-2 mt-2 cursor-pointer">
+      <div
+        className="bg-red-500 w-8 h-8 flex justify-center items-center rounded-full mr-2 mt-2 cursor-pointer"
+        onClick={_onDelete}
+      >
         <Image
           src="/img/delete-button.svg"
           width={16}
